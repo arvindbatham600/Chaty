5ce5a420fbe8aa1c74ec8cc10ca81144b2f83608
Arvind Batham <arvindbatham600.com>
2022-12-25 12:23:16 +0530

    change in package.json file

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
-
 const server = require("http").createServer(app);
+
+
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -17,9 +18,9 @@ server.listen(PORT, () => {
 const io = require("socket.io")(server);
 
 io.on("connection", (socket) => {
-    // console.log('a user connected');
-    socket.on("message", (msg) => {
-        // console.log(msg);
-        socket.broadcast.emit("message", msg);
-    })
+  // console.log('a user connected');
+  socket.on("message", (msg) => {
+    // console.log(msg);
+    socket.broadcast.emit("message", msg);
+  });
 });
